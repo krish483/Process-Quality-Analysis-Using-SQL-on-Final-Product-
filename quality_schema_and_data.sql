672bdd376fff95533eac85ce0791432e13442988
krish483 <146372343+krish483@users.noreply.github.com>
2025-09-02 17:46:50 +0530

    Update quality_schema_and_data.sql

--- quality_schema_and_data.sql
+++ quality_schema_and_data.sql
@@ -29,3 +29,86 @@ INSERT INTO ProductQuality (
 ('M70159', 16, 'FE550 D', 'C', '2025-01-01 01:20:00', 'A', 'Regular Sample', 598.0, 699.0, 1.169, 'Ok'),
 ('M70159', 16, 'FE550 D', 'C', '2025-01-01 01:20:00', 'B', 'Regular Sample', 607.0, 706.0, 1.162, 'Ok'),
 ('M70160', 16, 'FE550 D', 'C', '2025-01-01 01:50:00', 'A', 'Regular Sample', 614.0, 709.0, 1.155, 'Ok');
+
+
+-- Question 1 
+-- Which grade has the highest average Yield Strength (YS)?
+
+SELECT GRADE, AVG(YS) AS avg_ys
+FROM NBM_Data
+GROUP BY GRADE
+ORDER BY avg_ys DESC
+LIMIT 1;
+
+-- Question 2 
+-- What is the average UTS and YS per shift (A, B, C)?
+
+SELECT SHIFT, AVG(YS) AS avg_ys, AVG(UTS) AS avg_uts
+FROM NBM_Data
+GROUP BY SHIFT;
+
+--Question 3 
+--Which line (A or B) produces higher strength on average?
+
+SELECT LINE, AVG(YS) AS avg_ys, AVG(UTS) AS avg_uts
+FROM NBM_Data
+GROUP BY LINE;
+
+--Question 4 
+--How many samples had surface inspection issues (not 'Ok')?
+
+SELECT COUNT(*) AS defect_count
+FROM NBM_Data
+WHERE SURFACE_INSPECTION <> 'Ok';
+
+-- Question 5
+--What is the trend of average UTS over months?
+
+SELECT DATE_FORMAT(SAMPLE_TIME, '%Y-%m') AS month, AVG(UTS) AS avg_uts
+FROM NBM_Data
+GROUP BY month
+ORDER BY month;
+
+
+-- Question 6
+--Which grade and section combination gives the highest UTS/YS ratio?
+
+SELECT GRADE, SEC., AVG([UTS/YS]) AS avg_ratio
+FROM NBM_Data
+GROUP BY GRADE, SEC.
+ORDER BY avg_ratio DESC
+LIMIT 1;
+-- Question 7
+--How many samples were taken in each sampling condition?
+
+SELECT SAMPLING_CONDITION, COUNT(*) AS sample_count
+FROM NBM_Data
+GROUP BY SAMPLING_CONDITION;
+
+
+-- Question 8
+--What is the distribution of defects across shifts?
+
+SELECT SHIFT, COUNT(*) AS defect_count
+FROM NBM_Data
+WHERE SURFACE_INSPECTION <> 'Ok'
+GROUP BY SHIFT;
+    
+-- Question 9
+--Which cast ID had the highest UTS recorded?
+
+SELECT CAST_ID, MAX(UTS) AS max_uts
+FROM NBM_Data
+GROUP BY CAST_ID
+ORDER BY max_uts DESC
+LIMIT 1;
+
+-- Question 10
+--Monthly defect percentage for each line (A/B)?
+
+SELECT LINE,
+       DATE_FORMAT(SAMPLE_TIME, '%Y-%m') AS month,
+       COUNT(CASE WHEN SURFACE_INSPECTION <> 'Ok' THEN 1 END) * 100.0 / COUNT(*) AS defect_percentage
+FROM NBM_Data
+GROUP BY LINE, month
+ORDER BY month, LINE;
